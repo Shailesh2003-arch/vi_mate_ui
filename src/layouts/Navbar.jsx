c4b60f72ba216d3da70116dc7edeeb954dd6e0c3
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-06-28 00:01:41 +0530

    feat (navbar) : add navbar

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <header className="h-14 border-b border-zinc-800 bg-zinc-950 flex items-center px-4">
+      <h1 className="font-bold text-xl">
+        YouTube Clone
+      </h1>
+    </header>
+  );
+}
+
+export default Navbar;
