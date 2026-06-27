2df3b967eb8a175d5ab4090655a6bec2d0cd6e29
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-06-28 00:01:18 +0530

    feat (use-video) : add use-video hook

---
+++ src/hooks/useVideo.js
@@ -0,0 +1,37 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getVideoById } from "../services/video-service/VideoService.js";
+import { watchVideo } from "../services/video-service/VideoService.js";
+
+export const useVideo = (videoId) => {
+  return useQuery({
+    queryKey: ["video", videoId],
+
+    queryFn: () => getVideoById(videoId),
+
+    enabled: !!videoId,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useWatchVideo = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: watchVideo,
+    onSuccess: (_, videoId) => {
+      queryClient.setQueryData(["video", videoId], (oldData) => {
+        if (!oldData) return oldData;
+
+        return {
+          ...oldData,
+
+          video: {
+            ...oldData.video,
+
+            views: oldData.video.views + 1,
+          },
+        };
+      });
+    },
+  });
+};
