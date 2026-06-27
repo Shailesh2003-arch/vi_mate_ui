0b1ddfc9b07fb93be8c31aae087c8631c4efe8b0
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-06-28 00:00:38 +0530

    feat (main) : add App and Query-Client

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "./main.css";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+    <Toaster position="top-right" />
+    </QueryClientProvider>
+  </StrictMode>
 )
