7ebc03fd8ebcd1a0403c1faa1d146351eb6382a9
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-06-28 00:12:27 +0530

    feat (time-ago) : add utility feature for calculating post's posting time

---
+++ src/utils/timeAgo.js
@@ -0,0 +1,30 @@
+function timeAgo(dateString) {
+  const now = new Date();
+  const created = new Date(dateString);
+
+  const seconds = Math.floor((now - created) / 1000);
+
+  const minutes = Math.floor(seconds / 60);
+
+  const hours = Math.floor(minutes / 60);
+
+  const days = Math.floor(hours / 24);
+
+  const months = Math.floor(days / 30);
+
+  const years = Math.floor(days / 365);
+
+  if (years > 0) return `${years} year${years > 1 ? "s" : ""} ago`;
+
+  if (months > 0) return `${months} month${months > 1 ? "s" : ""} ago`;
+
+  if (days > 0) return `${days} day${days > 1 ? "s" : ""} ago`;
+
+  if (hours > 0) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+
+  if (minutes > 0) return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+
+  return "Just now";
+}
+
+export default timeAgo;
